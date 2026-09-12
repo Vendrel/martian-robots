e7bfecac98997f59d2246829b49bf5329304b74d
Endre Varga <varga.endre@gmail.com>
2026-09-12 17:42:05 +0200

    Configure rover image edge proxy

--- dist/runtime-config.js
+++ dist/runtime-config.js
@@ -1,3 +1,5 @@
 // Set this to the deployed rover-image-proxy URL.  Use '/nasa-image' if the
 // Worker is routed on the same domain as this app. Never place secrets here.
-window.MARS_ROVER_IMAGE_PROXY = { endpoint: '' };
+window.MARS_ROVER_IMAGE_PROXY = {
+  endpoint: 'https://edge-proxy.varga-endre.workers.dev'
+};
